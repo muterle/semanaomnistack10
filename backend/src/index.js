a0ab8d25abb422c0cef483feccbedda3f5a2c437
betto <betto@NOTE_BETTO>
2020-01-15 07:31:09 -0300

    First commit

---
+++ backend/src/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const routes = require('./routes');
+
+const app = express();
+
+mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-k649d.mongodb.net/week10?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333);
+
+//Metodos HTTP: GET: buscar, POST: inserir, PUT: alterar, DELETE: excluir
+
+// Tipos de parâmetros
+// Query Params: request.query (filtros, ordenação, paginação, ...)
+// Route Params: request.params (identificar um recurso na alteração ou remoção)
+// Body: request.body (dados para criação ou alteração)

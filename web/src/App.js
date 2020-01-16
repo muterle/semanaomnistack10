54774c9df031d5f346b80ce5bff3e92778284350
betto <betto@NOTE_BETTO>
2020-01-16 06:29:51 -0300

    Começando a criação do layout

--- web/src/App.js
+++ web/src/App.js
@@ -1,9 +1,12 @@
 import React, { useState } from 'react';
+import './global.css'
+import './App.css'
+import './Sidebar.css'
 
 // Componente: bloco isolado de HTML, CSS e JS, o qual não interfere no restante da aplicação.
 // Propriedade: como se fosse os atributos do HTML. Informações que o componente PAI passa para o componente FILHO
 // Estado: Informações mantidas pelo componente (Lembrar: imutabilidade)
-// Parei no minuto 42
+// minuto 59
 
 function App() {
 
@@ -14,10 +17,37 @@ function App() {
   }
 
   return (
-    <>
-      <h1>Contador: {counter}</h1>
-      <button onClick={incrementCounter}>Incrementar</button>
-    </>
+    <div id="app">
+      <aside>
+        <strong>Cadastrar</strong>
+        <form>
+          <div className="input-block">
+            <label htmlFor="github_username">Usuário do Github</label>
+            <input name="github_username" id="github_username" required />
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="techs">Tecnologias</label>
+            <input name="techs" id="techs" required />
+          </div>
+
+          <div className="input-group">
+            <div className="input-block">
+              <label htmlFor="latitude">Latitude</label>
+              <input name="latitude" id="latitude" required />
+            </div>
+            <div className="input-block">
+              <label htmlFor="longitude">Longitude</label>
+              <input name="longitude" id="longitude" required />
+            </div>
+          </div>
+          <button type="submit">Salvar</button>
+        </form>
+      </aside>
+      <main>
+
+      </main>
+    </div>
   );
 }
 
